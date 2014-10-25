8ef8837c584171a188f939cf770881a9fa288299
Shawn <shawn.bulger@spaceconcordia.ca>
2014-10-25 15:07:53 -0400

    Enabled opening log files in new console, wrote wrapper functions for opening new processes, need to implement commander echoes to pipe

--- Python/gs.py
+++ Python/gs.py
@@ -22,6 +22,8 @@ import stat
 import subprocess
 import time
 import sys
+import datetime # to return the current ISO date for selecting log files
+
 
 try:
     raw_input
@@ -31,24 +33,38 @@ except NameError:
 #-------------------------------------------------------------
 # GLOBALS 
 #-------------------------------------------------------------
+iso_today = datetime.datetime.now().strftime("%Y%m%d")
 ground_netman = None
 ground_commander = None
 mock_satellite_netman = None
 mock_satellite_commander = None
+log_window = None
 
-GS_BIN_PATH = "/usr/bin/";
+GS_BIN_PATH = "/usr/bin/"
 GS_PATH = {
-    "LOG"            : '/home/logs/gs.log',
-    "INPUT_PIPE"     : '/home/pipes/gnd-input',
-    "NETMAN"         : GS_BIN_PATH+'gnd',
-    "MOCK_SAT_NM"    : GS_BIN_PATH+'sat',
-    "MOCK_SAT_CMDR"  : GS_BIN_PATH+'space-commander',
-    "DECODE_RB"      : GS_BIN_PATH+'decode-command.rb',
-    "GETLOG_RB"      : GS_BIN_PATH+'getlog-command.rb',
-    "GETTIME_RB"     : GS_BIN_PATH+'gettime-command.rb',
-    "REBOOT_RB"      : GS_BIN_PATH+'reboot-command.rb',
-    "UPDATE_RB"      : GS_BIN_PATH+'update-command.rb',
-    "STEP2_RB"       : GS_BIN_PATH+'step2.rb'
+    "LOG"               : "/home/logs/gs.log",
+    "INPUT_PIPE"        : "/home/pipes/gnd-input",
+    "NETMAN"            : GS_BIN_PATH+"gnd",
+    "GROUND_COMMANDER"  : "ground-commander.py",
+    "MOCK_SAT_NM"       : GS_BIN_PATH+"sat",
+    "MOCK_SAT_CMDR"     : GS_BIN_PATH+"space-commander",
+    "DECODE_RB"         : GS_BIN_PATH+"decode-command.rb",
+    "GETLOG_RB"         : GS_BIN_PATH+"getlog-command.rb",
+    "GETTIME_RB"        : GS_BIN_PATH+"gettime-command.rb",
+    "REBOOT_RB"         : GS_BIN_PATH+"reboot-command.rb",
+    "UPDATE_RB"         : GS_BIN_PATH+"update-command.rb",
+    "STEP2_RB"          : GS_BIN_PATH+"step2.rb"
+}
+
+GS_LOG_PATH = "/home/logs/"
+GS_LOG_EXT = ".log"
+LOG_FILES = {
+    "NETMAN"            : GS_LOG_PATH+"GROUND_NETMAN"+iso_today+GS_LOG_EXT,
+    "GROUND_COMMANDER"  : GS_LOG_PATH+"GROUND_COMMANDER"+iso_today+GS_LOG_EXT,
+    "GROUND_RADIO"      : GS_LOG_PATH+""+iso_today+GS_LOG_EXT,
+    "MOCK_SAT_NM"       : GS_LOG_PATH+"Netman"+iso_today+GS_LOG_EXT,
+    "MOCK_SAT_CMDR"     : GS_LOG_PATH+"Commander"+iso_today+GS_LOG_EXT,
+    "MOCK_SAT_RADIO"    : GS_LOG_PATH+"HE100"+iso_today+GS_LOG_EXT
 }
 
 KW_GETTIME  = "010001313337"    #0x01 0x00 0x01 0x31 0x33 0x37
@@ -91,7 +107,6 @@ def subShell(args):
             executable="/bin/bash"
     )
 
-
 #-------------------------------------------------------------
 # VARIOUS SYSTEM WRAPPER FUNCTIONS 
 #-------------------------------------------------------------
@@ -126,18 +141,39 @@ def check_requirements():
         if ( os.path.isfile(requirement) ) or ( stat.S_ISFIFO(os.stat(requirement).st_mode) ):
             continue
         else : fail(key+" ("+requirement+") is not present!")
-            
+
+def start_process(process_name):
+  print "[NOTICE] "+process_name+" STARTING"
+  process = subP(GS_PATH[process_name])
+  if is_subprocess_running(process):
+    print "[NOTICE] "+process_name+" IS RUNNING"
+    return process
+  else:
+    print "[ERROR] "+process_name+" HAS STOPPED"
+
 def start_ground_netman():
-  print "[NOTICE] STARTING GROUND NETMAN"
-  return subP(GS_PATH["NETMAN"])
+  return start_process("NETMAN")
 
 def start_mock_satellite_netman() :
-  print "[NOTICE] STARTING MOCK SATELLITE NETMAN"
-  return subP(GS_PATH["MOCK_SAT_NM"])
+  return start_process("MOCK_SAT_NM")
 
 def start_mock_satellite_commander() :
-  print "[NOTICE] STARTING MOCK SATELLITE COMMANDER"
-  return subP(GS_PATH["MOCK_SAT_CMDR"])
+  return start_process("MOCK_SAT_CMDR")
+
+def start_ground_commander(): 
+  print "[NOTICE] GROUND COMMANDER STARTING..."
+  ground_commander = subP(['python', GS_PATH["GROUND_COMMANDER"]])
+  stdout, stderr = ground_commander.communicate()
+  print stdout
+  print stderr
+  return ground_commander
+
+  # TODO when real ground-commander exists
+  #if is_subprocess_running(ground_commander):
+  #    print "[NOTICE] GROUND COMMANDER IS RUNNING"
+  #    return ground_commander
+  #else:
+  #    print "[ERROR] GROUND COMMANDER HAS STOPPED"
 
 def is_subprocess_running(subprocess):
     if subprocess is None:
@@ -153,6 +189,7 @@ def tear_down() :
     global ground_commander
     global mock_satellite_netman
     global mock_satellite_commander
+    global log_window
     if ( ground_netman is not None ) and ( is_subprocess_running(ground_netman) ) :
         ground_netman.terminate()
         print "[NOTICE] Ground Netman was terminated"
@@ -164,6 +201,9 @@ def tear_down() :
         print "[NOTICE] Mock Satellite Netman was terminated"
     if ( mock_satellite_commander is not None ) and ( is_subprocess_running(mock_satellite_commander) ) :
         mock_satellite_commander.terminate()
+        print "[NOTICE] Mock Satellite Commander was terminated"
+    if ( log_window is not None ) and ( is_subprocess_running(log_window) ) :
+        log_window.terminate()
         print "[NOTICE] Mock Satellite Commander was terminated"
 
 def start_ground_station():
@@ -177,21 +217,28 @@ def start_ground_station():
   # start ground-commmander and drop to shell
   global ground_commander
   ground_commander = start_ground_commander()
+
 #end def
 
 def start_mock_interaction():
   global mock_satellite_netman
   mock_satellite_netman = start_mock_satellite_netman()
+  start_ground_station()
+  global log_window
+  log_window = open_log_window()
   global mock_satellite_commander
   mock_satellite_commander = start_mock_satellite_commander()
-  start_ground_station()
 
-def start_ground_commander(): 
-  print "[NOTICE] STARTING GROUND COMMANDER"
-  ground_commander = subP(['python', 'ground-commander.py'])
-  stdout, stderr = ground_commander.communicate()
-  print stdout
-  print stderr
+def open_log_window():
+    log_view_command = "tail -f"
+    subP(['ls', GS_LOG_PATH])
+    for key, logfile in LOG_FILES.items():
+        if ( os.path.isfile(logfile) ):
+            log_view_command+=" "+logfile
+        else : print key+" ("+logfile+") is not present!"
+    print "xterm -e "+log_view_command
+    return subP(['xterm', '-e', log_view_command])
+    #subprocess.call(['tmux', 'split-window', "'"+log_view_command+"'"])
 
 def go_no_go():
   global ground_netman
@@ -218,19 +265,10 @@ def command_line_interface():
 # GROUND STATION FUNCTIONS 
 #-------------------------------------------------------------
 def main():
-    # TODO not working #location = whereis('echo')
-    #if location is not None:
-    #    print location
-    #subShell(['echo', 'hello space'])
-    #process = subprocess.Popen(['echo', 'Hello World!'], shell=False, stdout=subprocess.PIPE)
-    #print process.communicate()
-    usage()
     check_requirements()
+    usage()
     command_line_interface()
-
-    # off
     tear_down()
-    # exit()
     exit("Reached end of main")
 
 if __name__ == '__main__':
