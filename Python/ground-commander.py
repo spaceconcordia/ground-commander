46a3578ac49787df22d25f91a4fda27fce7dc8fd
Shawn <shawn.bulger@spaceconcordia.ca>
2014-11-06 01:13:39 -0500

    Added gettime as a commander command to interact with satellite

--- Python/ground-commander.py
+++ Python/ground-commander.py
@@ -6,32 +6,35 @@
 #
 # Distributed under terms of the MIT license.
 
+#st - settime    set the satellite time
+#ud - update     update a binary on the satellite
+#gl - getlog     get a log file from the satellite
+#rb - reboot     reboot the satellite
+#dc - decode
+#dl - deletelog  delete a log from the satellite
+#tt - timetag    timetag a command on the satellite
+
 """
 This is a prototype of the ground station commander in Python to simulate certain commands 
-st - settime    set the satellite time
 gt - gettime    get the satellite time
-ud - update     update a binary on the satellite
-gl - getlog     get a log file from the satellite
-rb - reboot     reboot the satellite
-dc - decode
-dl - deletelog  delete a log from the satellite
-tt - timetag    timetag a command on the satellite
+cf - confirm    prompt the satellite to go ahead with the previous command
 q  - exit       and close all other ground station applications
 """
 
 import os
 
-settime   = bytearray.fromhex('30 21');
-gettime   = bytearray.fromhex('31 21');
-update    = bytearray.fromhex('32 21');
-getlog    = bytearray.fromhex('33 21');
-reboot    = bytearray.fromhex('34 21');
-decode    = bytearray.fromhex('35 21');
-deletelog = bytearray.fromhex('36 21');
-timetag   = bytearray.fromhex('37 21');
-
+settime   = bytearray.fromhex('30');
+gettime   = bytearray.fromhex('31');
+update    = bytearray.fromhex('32');
+getlog    = bytearray.fromhex('33');
+reboot    = bytearray.fromhex('34');
+decode    = bytearray.fromhex('35');
+deletelog = bytearray.fromhex('36');
+timetag   = bytearray.fromhex('37');
+confirm   = bytearray.fromhex('21');
 
 def usage():
     print(globals()['__doc__'])
+
 
 usage()
--- Python/gs.py
+++ Python/gs.py
@@ -71,6 +71,16 @@ KW_GETTIME  = "010001313337"    #0x01 0x00 0x01 0x31 0x33 0x37
 KW_CONFIRM  = "02000121242b"    #0x02 0x00 0x01 0x21 0x24 0x2b
 KW_ACK      = "31210052d5"      #0x31 0x21 0x00 0x52 0xd5
 
+settime   = bytearray.fromhex('30');
+gettime   = bytearray.fromhex('31');
+update    = bytearray.fromhex('32');
+getlog    = bytearray.fromhex('33');
+reboot    = bytearray.fromhex('34');
+decode    = bytearray.fromhex('35');
+deletelog = bytearray.fromhex('36');
+timetag   = bytearray.fromhex('37');
+confirm   = bytearray.fromhex('21');
+
 #-------------------------------------------------------------
 # WRAPPER FUNCTIONS FOR SUBPROCESS 
 #-------------------------------------------------------------
@@ -131,6 +141,12 @@ def exit(exit_message="No message"):
 def signal_handler(signal, frame):
   print '\r\nCaught Cntl+C!'
 signal.signal(signal.SIGINT, signal_handler)
+
+def send_command(command):
+    global confirm
+    input_pipe = open(GS_PATH["INPUT_PIPE"],"w")
+    input_pipe.write(command)
+    input_pipe.close()
 
 
 #-------------------------------------------------------------
@@ -246,13 +262,13 @@ def open_log_window():
 
 def go_no_go():
   global ground_netman
-  return "GO >> " if is_subprocess_running(ground_netman) is True else "NOGO >> "
+  return "GO" if is_subprocess_running(ground_netman) is True else "NOGO"
 
 def command_line_interface():
   print 'Enter commands for the ground station below.\r\nType "menu" for predefined commands, and "exit" to quit'
   input=1
   while 1:
-    input=raw_input( go_no_go() + time.strftime("%H:%M:%S") + " >> " )
+    input=raw_input( go_no_go() + " >> " + time.strftime("%H:%M:%S") + " >> " )
     if ((input == "exit") | (input == "q")):
       tear_down()
       exit("Because you asked to.")    
@@ -264,6 +280,11 @@ def command_line_interface():
       start_mock_interaction()
     if ((input == "teardown") | (input == "td") | (input == "t")):
       tear_down()
+    if ( is_subprocess_running(mock_satellite_commander) ):
+        if (( input == "gt" ) | (input == "gettime")):
+            send_command(gettime)
+        if (( input == "cf" ) | (input == "confirm")):
+            send_command(confirm)
 
 #-------------------------------------------------------------
 # GROUND STATION FUNCTIONS 
